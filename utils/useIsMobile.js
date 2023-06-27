d7ea074e2dfd6495a0299575673ee41d5e1525ef
Keith Humphrey <keith@christianexodus.org>
2023-06-27 16:42:36 -0700

    change to mobile display at 720

--- utils/useIsMobile.js
+++ utils/useIsMobile.js
@@ -38,7 +38,7 @@ export const useWindowSize = () => {
 export const useIsMobile = () => {
     const isSsrMobile = useContext(IsSsrMobileContext);
     const { width: windowWidth } = useWindowSize();
-    const isBrowserMobile = !!windowWidth && windowWidth < 460;
+    const isBrowserMobile = !!windowWidth && windowWidth < 720;
 
     return isSsrMobile || isBrowserMobile;
 };
