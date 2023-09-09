a43e95dcf54919f29dd17be088de4e4078b0e166
Keith Humphrey <keith@christianexodus.org>
2023-09-09 08:48:23 -0700

    useEffect

--- pages/visitMetrics.js
+++ pages/visitMetrics.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { axiosClient } from '../axiosClient'
 import TopBar from '../components/AdminTopBar'
 import cookie from 'js-cookie'
